f739e03407e94cb6c4b90440673597bbdb8821fb
Yafet Berhanu <yafetberhanu3@gmail.com>
2021-07-09 01:11:47 -0700

    On branch master

---
+++ exams-conductor.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+require_once('config.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+  <p>Exams</p>
+  <?php
+  $examQuery="SELECT * FROM examination ":
+  ?>
+</body>
+</html>
